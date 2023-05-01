cc196e06a014ed90ba8972e30c2b1fa7e3c296e6
Sarahsegla <sarahsegla@icloud.com>
2023-05-02 00:43:18 +0100

    added icons but need to inline

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./styles.css";
+import Button from '@mui/material/Button';
+import MailIcon from '@mui/icons-material/Mail';
+import AddIcon from '@mui/icons-material/Add';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Navbar () {
     return (
         <div>
-            <p>nav</p>
+            <div className="navbar">
+                <div className="page">
+                <Button className="home" variant="text" style={{ textDecoration: 'none', color: 'black', backgroundColor: 'gray' }}>Home</Button>
+                <Button variant="text" style={{ textDecoration: 'none', color: 'black' }}>Restaurants</Button>
+                <Button variant="text" style={{ textDecoration: 'none', color: 'black' }}>Contact</Button>
+                <div className="icons">
+                    <MailIcon/>
+                    <AddIcon/>
+                    <InstagramIcon/>
+                    <SearchIcon/>
+                </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
