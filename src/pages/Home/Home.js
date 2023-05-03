135cc204a0d53141ffccc33e1985b1d3a3812787
Sarahsegla <sarahsegla@icloud.com>
2023-05-03 14:57:12 +0100

    added the name brand

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css"
+
+function Home () {
+    return (
+        <div>
+            <div className="header">
+                <h1 className="name" style={{  fontFamily: 'Bebas Neue' }}>DeffoDelish</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
