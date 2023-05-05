df37ea99a839f4bc39a898d2e808234788baaefc
Sarahsegla <sarahsegla@icloud.com>
2023-05-05 20:23:45 +0100

    in center

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Paper, Button } from '@mui/material';
+import "./styles.css";
 
 function Item(props)
 {
     return (
         <Paper>
-            <img src={props.item.image} alt={props.item.description} />
+            <div className="mySlides" style={{textAlign: 'center'}}>
+            <img src={props.item.image} alt={props.item.description} style={{width: '800', height: '800' }}/>
             <h1>{props.item.description}</h1>
+            </div>
 
             <Button className="CheckButton">
                 Check it out!
