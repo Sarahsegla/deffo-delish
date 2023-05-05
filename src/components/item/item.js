296c96925b198e139e0ea41e87635ecb75689d56
Sarahsegla <sarahsegla@icloud.com>
2023-05-06 00:39:20 +0100

    need to fix about class

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -7,12 +7,12 @@ function Item(props)
     return (
         <Paper>
             <div className="mySlides" style={{textAlign: 'center'}}>
-            <img src={props.item.image} alt={props.item.description} style={{width: '800', height: '800' }}/>
+            <img src={props.item.image} alt={props.item.description} style={{width: '700', height: '500', paddingTop: '10px' }}/>
             <h1>{props.item.description}</h1>
             </div>
 
             <Button className="CheckButton">
-                Check it out!
+                {/* Check it out! */}
             </Button>
         </Paper>
     )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,12 +6,18 @@ function Home () {
     return (
         <div>
             <div className="header">
-                <h1 className="name" style={{  fontFamily: 'Bebas Neue', float: 'left' }}>DeffoDelish</h1>
+                <h1 className="name" style={{  fontFamily: 'Bebas Neue', float: 'left', padding: '10px' }}>DeffoDelish</h1>
             </div>
+            <article>
+                <div className="about" style={{textAlign: 'left'}}>
+                    Food and Travel Blogger.
+                    Food From All Over the World.
+                    Any Suggestion of Restaurants?
+                </div>
+            </article>
             <div className="slides">
                 <Carousel />
-
-            </div>
+                </div>
         </div>
     )
 }
