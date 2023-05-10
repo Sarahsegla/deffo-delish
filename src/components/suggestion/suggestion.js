99bc098bb4567274f4a2f7772b782e0500724de6
Sarahsegla <sarahsegla@icloud.com>
2023-05-11 00:56:50 +0100

    added animation

--- src/components/suggestion/suggestion.js
+++ src/components/suggestion/suggestion.js
@@ -1,14 +1,21 @@
 import * as React from 'react';
+import { useEffect } from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Gif1 from '../../image/spag1.gif';
 import Gif2 from '../../image/options.gif';
 import Gif3 from '../../image/shop.gif';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function SimpleContainer() {
+  useEffect(()=> {
+    AOS.init({duration: 2000})
+}, [])
   return (
     <>
-        <h3 style={{textAlign: 'left', paddingTop: '100px', paddingBottom: '100px', paddingLeft: '50px', fontSize: '30px'}}>Any Suggestion of Restaurants?</h3>
+        <h3 data-aos="fade-up-right" style={{textAlign: 'left', paddingTop: '100px', paddingBottom: '100px', paddingLeft: '50px', fontSize: '30px'}}>
+          Any Suggestion of Restaurants?</h3>
         
         <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={3}>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
 import "./styles.css";
 import Carousel from "../../components/carousel/carousel";
 import Jumbo from "../../components/jumbo/jumbo";
 import Suggestion from "../../components/suggestion/suggestion";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home () {
+    useEffect(()=> {
+        AOS.init({duration: 2000})
+    }, [])
     return (
         <div>
              <div className="subhead" style={{ paddingTop: '70px', paddingLeft: '50px', fontSize: '30px'}}>
                 <h2 style={{display: 'inline'}}> 
                 <span id="we">We</span> Don't Cook, <span id="we">We</span> Eat.</h2>
              </div>
-            <header className="header">
+            <header className="header" data-aos="fade-up" >
            <Jumbo/>
             </header>
             <article>
-            <div className="about" style={{textAlign: 'left', paddingTop: '80px', paddingBottom: '10px', paddingLeft: '50px', fontSize: '30px'}}> 
+            <div className="about" data-aos="fade-up-right" style={{textAlign: 'left', paddingTop: '80px', paddingBottom: '10px', paddingLeft: '50px', fontSize: '30px'}}> 
             <h3>
                     Food and Travel Blogger.
                     <br></br>
                     Food From All Over the World.
-                    {/* <br></br>
-                    Any Suggestion of Restaurants? */}
-                    </h3>
+                   </h3>
                 </div>
             </article>
             <div className="slides" style={{paddingBottom: '220px', backgroundColor: '#5072A7'}}>
