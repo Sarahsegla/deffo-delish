74d58537101ec45614ad2e2b227bb5c117976dc1
Sarahsegla <sarahsegla@icloud.com>
2023-05-11 22:10:36 +0100

    only one gif and need to make contact form

--- src/components/suggestion/suggestion.js
+++ src/components/suggestion/suggestion.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { useEffect } from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Gif1 from '../../image/spag1.gif';
+// import Gif1 from '../../image/spag1.gif';
 import Gif2 from '../../image/options.gif';
-import Gif3 from '../../image/shop.gif';
+// import Gif3 from '../../image/shop.gif';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -18,15 +18,16 @@ export default function SimpleContainer() {
           Any Suggestion of Restaurants?</h3>
         
         <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={3}>
-          <Grid item xs>
+        <Grid container spacing={2}>
+          {/* <Grid item xs>
             <img src={Gif1} alt='spaghetti' style={{width: '350px', height: '250px'}}/>
-          </Grid>
-          <Grid item xs>
+          </Grid> */}
+          <Grid item xs style={{paddingLeft: '70px'}}>
           <img src={Gif2} alt='option of food'style={{width: '350px', height: '250px'}}/>
           </Grid>
           <Grid item xs>
-          <img src={Gif3} alt='shops' style={{width: '350px', height: '250px'}}/>
+          {/* <img src={Gif3} alt='shops' style={{width: '350px', height: '250px'}}/> */}
+          <p>contact form here</p>
           </Grid>
         </Grid>
       </Box>
